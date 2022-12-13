97f6eca79f568a81efd40c6607ce413fe9b5b111
Karuppasamy Kumar S <kumars@dynamicpdf.com>
2022-12-13 12:39:50 +0530

    PageSize and PageOrientation added in pageInput

--- DynamicPDF.Api/PageInput.cs
+++ DynamicPDF.Api/PageInput.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using System.Collections.Generic;
+using api = DynamicPDF.Api;
 
 namespace DynamicPDF.Api
 {
@@ -12,6 +13,29 @@ namespace DynamicPDF.Api
     public class PageInput : Input
     {
         private List<Element> elements;
+        private api.PageSize pageSize = api.PageSize.A4;
+        private api.PageOrientation pageOrientation = api.PageOrientation.Portrait;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageInput"/> class.
+        /// </summary>
+        /// <param name="size">The size of the page.</param>
+        /// <param name="orientation">The orientation of the page.</param>
+        /// <param name="margins">The margins of the page.</param>
+        public PageInput(api.PageSize size = PageSize.A4, api.PageOrientation orientation = api.PageOrientation.Portrait, float? margins = null) 
+        { 
+            if(orientation != api.PageOrientation.Portrait)
+                PageOrientation = orientation;
+            if(size != PageSize.A4) 
+                PageSize = size;
+            if(margins != null)
+            {
+                TopMargin = margins;
+                BottomMargin = margins;
+                RightMargin = margins;
+                LeftMargin = margins;
+            }
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PageInput"/> class.
@@ -20,10 +44,10 @@ namespace DynamicPDF.Api
         /// <param name="pageHeight">The height of the page.</param>
         public PageInput(float pageWidth, float pageHeight) { PageWidth = pageWidth; PageHeight = pageHeight; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PageInput"/> class.
-        /// </summary>
-        public PageInput() { }
+        ///// <summary>
+        ///// Initializes a new instance of the <see cref="PageInput"/> class.
+        ///// </summary>
+        //public PageInput() { }
 
         [JsonProperty("type")]
         [JsonConverter(typeof(StringEnumConverter), converterParameters: typeof(CamelCaseNamingStrategy))]
@@ -38,6 +62,83 @@ namespace DynamicPDF.Api
         /// Gets or sets the height of the page.
         /// </summary>
         public float? PageHeight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the top margin.
+        /// </summary>
+        public float? TopMargin { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the left margin.
+        /// </summary>
+        public float? LeftMargin { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the bottom margin.
+        /// </summary>
+        public float? BottomMargin { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the right margin.
+        /// </summary>
+        public float? RightMargin { get; set; } = null;
+
+        /// <summary>
+        /// Gets or sets the Page size.
+        /// </summary>
+        [JsonIgnore]
+        public api.PageSize PageSize
+        {
+            set
+            {
+                pageSize = value;
+                double pgWidth = 0.0f;
+                double pgHeight = 0.0f;
+                UnitConverter.GetPaperSize(value, out pgWidth, out pgHeight);
+                if (PageOrientation == api.PageOrientation.Portrait)
+                {
+                    PageHeight = (float)pgHeight;
+                    PageWidth = (float)pgWidth;
+                }
+                else
+                {
+                    PageHeight = (float)pgWidth;
+                    PageWidth = (float)pgHeight;
+                }
+            }
+            get
+            {
+                return pageSize;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets page orientation.
+        /// </summary>
+        [JsonIgnore]
+        public api.PageOrientation PageOrientation
+        {
+            get
+            {
+                return pageOrientation;
+            }
+            set
+            {
+                pageOrientation = value;
+                if (pageOrientation == api.PageOrientation.Landscape)
+                {
+                    float? tempWidth = PageWidth;
+                    if (PageWidth != null)
+                    {
+                        PageWidth = PageHeight;
+                    }
+                    if (PageHeight != null)
+                    {
+                        PageHeight = tempWidth;
+                    }
+                }
+            }
+        }
 
         /// <summary>
         /// Gets or sets the elements of the page.
