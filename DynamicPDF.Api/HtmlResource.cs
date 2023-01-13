1e80bb958ab9616140a9f144f7ef72645f0148f8
Karuppasamy Kumar S <kumars@dynamicpdf.com>
2023-01-13 16:44:19 +0530

    Andy final discussion changes.

--- DynamicPDF.Api/HtmlResource.cs
+++ DynamicPDF.Api/HtmlResource.cs
@@ -16,26 +16,11 @@ namespace DynamicPDF.Api
         /// <summary>
         /// Initializes a new instance of the <see cref="HtmlResource"/> class.
         /// </summary>
-        /// <param name="filePathOrString">The html file path or the html string.</param>
+        /// <param name="html">The input html string.</param>
         /// <param name="resourceName">The name of the resource.</param>
-        public HtmlResource(string filePathOrString, string resourceName = null) : base() 
+        public HtmlResource(string html, string resourceName = null) : base() 
         {
-            string extension = filePathOrString.Substring(filePathOrString.Length - 5);
-            string dosExtension = extension.Substring(1);
-            if (extension.ToLower().Equals(".html") || dosExtension.ToLower().Equals(".htm"))
-            {
-                if (File.Exists(filePathOrString))
-                {
-                    Data = Resource.GetFileData(filePathOrString);
-                    FilePath = filePathOrString;
-                }
-                else
-                    throw new EndpointException("File does not exist.");      
-            }
-            else
-            {
-                Data = Encoding.UTF8.GetBytes(filePathOrString); 
-            }
+            Data = Encoding.UTF8.GetBytes(html); 
             if (resourceName == null)
                 ResourceName = Guid.NewGuid().ToString() + FileExtension;
             else
