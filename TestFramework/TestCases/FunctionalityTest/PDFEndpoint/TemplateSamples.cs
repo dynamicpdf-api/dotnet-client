485f56ec0b1a8de0a42819780bdd38f0185b453c
Kaviprakash <kaviprakash@dynamicpdf.com>
2023-11-16 13:15:07 +0530

    Testcase added for F1490

--- TestFramework/TestCases/FunctionalityTest/PDFEndpoint/TemplateSamples.cs
+++ TestFramework/TestCases/FunctionalityTest/PDFEndpoint/TemplateSamples.cs
@@ -59,5 +59,50 @@ namespace DynamicPDFApiTestForNET.TestCases.FunctionalityTest.PDFEndpoint
             }
             Assert.IsTrue(pass);
         }
+        [TestMethod]
+        public void TestPdfProducer_Pdfoutput()
+        {
+            Name = "TestPdfProducer";
+
+            Pdf pdf = new Pdf();
+            pdf.Author = Author;
+            pdf.Title = Title;
+            pdf.Producer = "ceTe";
+
+            PdfResource resource = new PdfResource(base.GetResourcePath(@"DocumentA100.pdf"));
+            PdfInput input = new PdfInput(resource);
+
+            pdf.Inputs.Add(input);
+
+            Template template = new Template("Temp1");
+            TextElement element = new TextElement("Hello World",ElementPlacement.TopCenter);
+            element.XOffset = 0;
+            element.YOffset = 0;
+            element.Color = RgbColor.Green;
+            element.FontSize = 20;
+            element.Font = Font.TimesRoman;
+            template.Elements.Add(element);
+            input.Template = template;
+
+            PdfResponse response = pdf.Process();
+
+            bool pass = false;
+
+            if (response.IsSuccessful)
+            {
+                File.WriteAllBytes(base.GetOutputFilePath("Output.pdf", InputSampleType), (byte[])response.Content);
+
+#if BASELINEREQUIRED
+                // Uncomment the line below to recreate the Input PNG Images
+                base.CreateInputPngsFromOutputPdf(72, InputSampleType);
+
+                pass = base.CompareOutputPdfToInputPngs(72, InputSampleType);
+#else
+                pass = response.IsSuccessful;
+#endif
+
+            }
+            Assert.IsTrue(pass);
+        }
     }
 }
